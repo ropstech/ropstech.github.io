999d429a45a5e85ed410c2177e367ca412d880fa
bangerang-rob <robin.spalthoff@gmail.com>
2023-03-17 20:47:33 +0100

    try text_scramble.js

---
+++ scripts/mouseover.js
@@ -0,0 +1,19 @@
+// ——————————————————————————————————————————————————
+// MouseOver
+// ——————————————————————————————————————————————————
+
+document.body.addEventListener("mousemove", evt => {
+    const mouseX = evt.clientX;
+    const mouseY = evt.clientY;
+    
+    gsap.set(".cursor", {
+        x: mouseX,
+        y: mouseY });
+    
+    
+    gsap.to(".shape", {
+        x: mouseX,
+        y: mouseY,
+        stagger: -0.1 });
+    
+    });
